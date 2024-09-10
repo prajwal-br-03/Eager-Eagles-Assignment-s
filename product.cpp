dfd3914bef48ecb8eb4865534f3fe38febef8722
Prajwal B R <prajwalbr30@gmail.com>
2024-09-10 16:38:32 +0530

    added product file

---
+++ product.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	cout<<"select a option in the menu"<<endl;
+	cout<<"Enter 1 to add a product"<<endl;
+	cout<<"Enter 2 to update quantity"<<endl;
+	cout<<"Enter 3 to search items"<<endl;
+}
