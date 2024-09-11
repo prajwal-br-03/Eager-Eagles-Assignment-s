e1b0b943f4ab7be70301049e7e654e3ddc37a855
Prajwal B R <prajwalbr30@gmail.com>
2024-09-11 10:48:14 +0530

    added some functions to product file

--- product.cpp
+++ product.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(){
-	cout<<"select a option in the menu"<<endl;
-	cout<<"Enter 1 to add a product"<<endl;
-	cout<<"Enter 2 to update quantity"<<endl;
-	cout<<"Enter 3 to search items"<<endl;
+struct product{
+	int id;
+	string name;
+	string category;
+	int quantity;
 }
+
+
+int main(){
+
+	
+}
+void menu(){
+	int choice;
+	cout<<"please choose an option: "<<endl;
+	cout<<"Enter 1 to add an item"<<endl;
+	cout<<"Enter 2 to change quantity of product"<<endl;
+	cout<<"Enter 3 to search for an item"<<endl;
+	cout<<"Press 4 to exit"<<endl;
+
+	cin>>choice;
+}
+void additem(products items[],&counter){
+
+
